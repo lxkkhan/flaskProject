2e5698e2a35f2105eddda7d18ec7685250915842
lxkkhan <17602264257@163.com>
2023-07-25 22:43:06 +0800

    backend更新2023年7月25日22:35:34

--- src/backend.py
+++ src/backend.py
@@ -12,7 +12,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello World Love!'
+    return 'Hello World Love Yesterday!'
 
 
 if __name__ == '__main__':
