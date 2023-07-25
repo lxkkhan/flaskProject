04a5a455937d8213e292457f50ab0e5895247745
lxkkhan <17602264257@163.com>
2023-07-25 22:32:30 +0800

    测试平台2023年7月25日22:30:28

---
+++ src/backend.py
@@ -0,0 +1,19 @@
+"""
+-*- coding:utf-8 -*-
+@File: backend.py
+@Author: kkhan
+@DateTime: 2023/7/25 22:03
+@Description:
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():  # put application's code here
+    return 'Hello World!'
+
+
+if __name__ == '__main__':
+    app.run()
