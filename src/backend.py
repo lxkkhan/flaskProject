942acf54ff68ab9c7f261f453d45bfb417f67bc9
lxkkhan <17602264257@163.com>
2023-07-25 23:07:19 +0800

    脚手架建设2023年7月25日23:07:14

--- src/backend.py
+++ src/backend.py
@@ -6,14 +6,51 @@
 @Description:
 """
 from flask import Flask
+from flask_restful import Resource, Api
 
 app = Flask(__name__)
+api = Api(app)
 
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World Love Yesterday!'
+# @app.route('/')
+# def hello_world():  # put application's code here
+#     return 'Hello World Love Yesterday!'
 
+# 首页
+class Main(Resource):
+    def get(self):
+        return {'hello': 'world'}
+
+
+# 用户管理
+class UserApi(Resource):
+    def get(self):
+        return {'hello': 'world'}
+
+
+# 测试用例管理
+class TestCaseApi(Resource):
+    def get(self):
+        return {'hello': '测试用例'}
+
+
+# 任务管理
+class TaskApi(Resource):
+    def get(self):
+        return {'hello': '任务管理'}
+
+
+# 报告管理
+class ReportApi(Resource):
+    def get(self):
+        return {'hello': '报告管理'}
+
+
+api.add_resource(Main, '/')
+api.add_resource(UserApi, '/login')
+api.add_resource(TestCaseApi, '/testcase')
+api.add_resource(TaskApi, '/task')
+api.add_resource(ReportApi, '/report')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
